5af94bcf13aeca003bc22d3a958bfe5967bea69c
Nikolai Patrick <nikolaipatricknp@gmail.com>
2022-08-24 09:32:05 +0930

    Create base plugin

---
+++ src/main/java/com/nikolaipatrick/minigamesmanager2/MinigamesManager2.java
@@ -0,0 +1,16 @@
+package com.nikolaipatrick.minigamesmanager2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinigamesManager2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Starting MinigamesManager 1.0.0");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
