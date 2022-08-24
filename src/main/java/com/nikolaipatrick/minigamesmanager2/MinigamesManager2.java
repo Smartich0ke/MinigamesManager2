ab971348d4145f2cfa8fc7c0368ac802014a1143
Nikolai Patrick <nikolaipatricknp@gmail.com>
2022-08-24 09:53:53 +0930

    create command classes

--- src/main/java/com/nikolaipatrick/minigamesmanager2/MinigamesManager2.java
+++ src/main/java/com/nikolaipatrick/minigamesmanager2/MinigamesManager2.java
@@ -1,12 +1,24 @@
 package com.nikolaipatrick.minigamesmanager2;
 
+import com.nikolaipatrick.minigamesmanager2.commands.mm_set;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.nikolaipatrick.minigamesmanager2.commands.mm_create;
+import com.nikolaipatrick.minigamesmanager2.commands.mm_delete;
+import com.nikolaipatrick.minigamesmanager2.commands.mm_list;
+import com.nikolaipatrick.minigamesmanager2.commands.mm_add;
+import com.nikolaipatrick.minigamesmanager2.commands.minigame;
 
 public final class MinigamesManager2 extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getLogger().info("Starting MinigamesManager 1.0.0");
+        getCommand("mm-create").setExecutor(new mm_create());
+        getCommand("mm-delete").setExecutor(new mm_delete());
+        getCommand("mm-list").setExecutor(new mm_list());
+        getCommand("mm-add").setExecutor(new mm_add());
+        getCommand("mm-set").setExecutor(new mm_set());
+        getCommand("minigame").setExecutor(new minigame());
     }
 
     @Override
---
+++ src/main/java/com/nikolaipatrick/minigamesmanager2/commands/mm_create.java
@@ -0,0 +1,13 @@
+package com.nikolaipatrick.minigamesmanager2.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class mm_create implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
