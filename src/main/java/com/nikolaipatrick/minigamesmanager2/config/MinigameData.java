92f09c5a7d1f70e02a1c9dd2ab23a0d98de5244e
Nikolai Patrick <nikolaipatricknp@gmail.com>
2022-08-25 17:32:11 +0930

    add a minigame profile creation method

--- src/main/java/com/nikolaipatrick/minigamesmanager2/config/MinigameData.java
+++ src/main/java/com/nikolaipatrick/minigamesmanager2/config/MinigameData.java
@@ -8,15 +8,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class MinigameData {
-public static void createDataFile(){
-    YamlDocument minigame_data;
-    try {
-        minigame_data = YamlDocument.create(new File("plugins/MinigamesManager2/minigame_data.yml"), MinigamesManager2.getPlugin(MinigamesManager2.class).getResource("minigame_data.yml"));
-    } catch (IOException e) {
-        throw new RuntimeException(e);
-    }
-}
+    static YamlDocument minigameData;
+    public static void createDataFile(){
 
+        try {
+            minigameData = YamlDocument.create(new File("plugins/MinigamesManager2/minigame_data.yml"), MinigamesManager2.getPlugin(MinigamesManager2.class).getResource("minigame_data.yml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void createMinigameProfile(String minigameName){
+        minigameData.set("minigames." + minigameName + ".name", minigameName);
+    }
 }
 
 
