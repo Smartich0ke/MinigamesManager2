90dca3afc41de946914b1e745c1110fee9c34158
Nikolai Patrick <nikolaipatricknp@gmail.com>
2022-08-26 09:34:01 +0930

    add a delete command

--- src/main/java/com/nikolaipatrick/minigamesmanager2/commands/mm_delete.java
+++ src/main/java/com/nikolaipatrick/minigamesmanager2/commands/mm_delete.java
@@ -1,5 +1,8 @@
 package com.nikolaipatrick.minigamesmanager2.commands;
 
+import com.nikolaipatrick.minigamesmanager2.config.MinigameData;
+import com.nikolaipatrick.minigamesmanager2.config.config;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,6 +10,24 @@ import org.bukkit.command.CommandSender;
 public class mm_delete implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(sender.hasPermission("minigamesmanager.delete.minigame")){
+            if(args.length > 2){
+                sender.sendMessage(config.configFile.getString("prefix") + ChatColor.RED + "Too many args!!");
+                return true;
+            }
+            if(args.length < 2) {
+                sender.sendMessage(config.configFile.getString("prefix") + ChatColor.RED + "not enough args!");
+                return true;
+            }
+            if (args[0].equals("minigame")) {
+                MinigameData.deleteMinigameProfile(args[1]);
+                sender.sendMessage(config.configFile.getString("prefix") + ChatColor.GREEN + "Minigame" + args[1] + "deleted successfully!");
+                return true;
+            }
+        } else {
+            sender.sendMessage(config.configFile.getString("deny-message"));
+            return true;
+        }
         return false;
     }
 }
