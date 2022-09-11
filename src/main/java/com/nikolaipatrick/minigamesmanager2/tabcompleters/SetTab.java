4bd30a2fdd50ada2d0ae4f7252eee5b63bb8433f
Nikolai Patrick <nikolaipatricknp@gmail.com>
2022-09-11 15:06:44 +0930

    hashSets and.. stuff ..?

--- src/main/java/com/nikolaipatrick/minigamesmanager2/tabcompleters/SetTab.java
+++ src/main/java/com/nikolaipatrick/minigamesmanager2/tabcompleters/SetTab.java
@@ -1,18 +1,13 @@
 package com.nikolaipatrick.minigamesmanager2.tabcompleters;
 
 import com.nikolaipatrick.minigamesmanager2.config.MinigameData;
-import com.nikolaipatrick.minigamesmanager2.config.config;
-import com.sun.org.apache.xerces.internal.xs.StringList;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class SetTab implements TabCompleter {
 
@@ -44,7 +39,9 @@ public class SetTab implements TabCompleter {
             }
             if (args[0].equals("tp-points")){
                 result.clear();
-                return (List<String>) MinigameData.minigameDataFile.getSection("minigames").getRoutesAsStrings(false);
+                Set<String> minigamesSet = new HashSet<>();
+                minigamesSet = MinigameData.minigameDataFile.getSection("minigames").getRoutesAsStrings(false);
+                return new ArrayList<>(minigamesSet);
             }
         }
         return null;
