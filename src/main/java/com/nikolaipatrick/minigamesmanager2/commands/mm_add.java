9bfbb5c8df3a3f6e98a7b87315f968f76c5dc953
Nikolai Patrick <nikolaipatricknp@gmail.com>
2022-09-12 06:52:42 +0930

    finish mm-add command

--- src/main/java/com/nikolaipatrick/minigamesmanager2/commands/mm_add.java
+++ src/main/java/com/nikolaipatrick/minigamesmanager2/commands/mm_add.java
@@ -8,14 +8,21 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class mm_add implements CommandExecutor {
+    //mm-add world <minigame> <lobby/arena> <world-name>
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender.hasPermission("minigamesmanager.add") || sender.isOp()) {
             if (args[0].equals("world")){
-                MinigameData.addMinigameWorld(args[1], args[3], args[2]);
-                sender.sendMessage(config.messagePrefix() + ChatColor.GREEN + "World " + ChatColor.YELLOW + args[3] + ChatColor.GREEN + " added as a " + ChatColor.YELLOW + args[2] + ChatColor.GREEN + " to the minigame " + ChatColor.YELLOW + args[1] + ChatColor.GREEN + " successfully!");
-                return true;
+                if(args[1].equals("lobby") || args[1].equals("arena")) {
+                    MinigameData.addMinigameWorld(args[1], args[3], args[2]);
+                    sender.sendMessage(config.messagePrefix() + ChatColor.GREEN + "World " + ChatColor.YELLOW + args[3] + ChatColor.GREEN + " added as a " + ChatColor.YELLOW + args[2] + ChatColor.GREEN + " to the minigame " + ChatColor.YELLOW + args[1] + ChatColor.GREEN + " successfully!");
+                    return true;
+                } else {
+                    sender.sendMessage(config.messagePrefix() + ChatColor.RED +"Specify a arena or lobby!");
+                    return true;
+                }
+
             }
             else {
                 return false;
--- src/main/java/com/nikolaipatrick/minigamesmanager2/commands/mm_set.java
+++ src/main/java/com/nikolaipatrick/minigamesmanager2/commands/mm_set.java
@@ -6,7 +6,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public class mm_set implements CommandExecutor {
     @Override
